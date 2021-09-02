1d74a75807bc8c6ac151b52c926d88c4de22425f
EdoardoRagazzi <edo.ragazzi@gmail.com>
2021-09-02 23:51:30 +0200

    Product with discount

--- Admin.php
+++ Admin.php
@@ -6,10 +6,21 @@ require_once 'User.php';
 class Admin extends User {
     public $authorization;
     public $password;
-    function __construct($_name,$_surname,$_discount,$_authorization,$_password){
+    public $grade;
+
+    function __construct($_name,$_surname,$_discount,$_authorization,$_password,$_grade){
         //Way to call a construct of file User.php just on the 2 variables checked in this case name and surname
         parent::__construct($_name,$_surname,$_discount);
         $this->authorization = $_authorization;
         $this->password = $_password;
+        $this->grade = $_grade;
+    }
+    //Function Polimorfismo--> change of function with diufferent value
+    public function makeSale($age){
+        if($age > 65){
+           return $this->discount = 60;
+        } else {
+           return $this->discount = $this->grade * 5;
+        }
     }
 }
--- index.php
+++ index.php
@@ -1,16 +1,21 @@
 <?php
-
+require_once 'Product.php';
 //Include the file User.php 
 // require_once 'User.php';
 require_once 'Admin.php';
 //Creating an instance called edo with the variables added
 $edo = new User('Edo','Ragazzi',0);
-
+$console = new Product(23, 'Playstation' , 300 , 'console game' );
 //Instance Admin include User
-$admin = new Admin( 'marco', 'baggio',0,'positive','edbd');   
+$admin = new Admin( 'marco', 'baggio',0,'positive','edbd',3);  
+
 //Calling the function
 $edo->makeSale(70);
 $sale = $edo->makeSale(70);
+
+//Calling function makeSale wiuth adding of grade for admin;
+$admin->makeSale(66 );    
+$saleAdmin = $admin->makeSale(66);
 
 ?>
 
@@ -29,9 +34,12 @@ $sale = $edo->makeSale(70);
  <!-- Calling variables of User Class ($edo->name . ' ' . $edo->surname) --- OR ---- calling the function GetFullName directly from User methods -->
 
                     <h1>Nome Utente: <?php echo $edo->getFullName();   ?></h1>
-                    <p><?php echo 'Hai uno sconto del :' . ' ' . $sale ?></p>
 
-                    <p>Admin Server: <?php echo $admin->authorization . ' & password :' . $admin->password ?> </p>
+                    <p> <?php echo 'Hai uno sconto del :' . ' ' . $sale . ' ' . 'su una '. ' ' . $console->name . ' ' . ($console->price - $saleAdmin)?></p>
+
+                    <p>Admin Server: <?php echo $admin->authorization . ' & password :' . $admin->password .' '. 'con uno sconto del: '. $saleAdmin?> </p>
+
+
     
 </body>
 </html>
